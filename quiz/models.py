01edc32a54d1f03d236f3ab5c66b01c6da779489
KissAniko <126045521+KissAniko@users.noreply.github.com>
2023-11-28 19:02:18 +0100

    initial commit

---
+++ quiz/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+# Create your models here.
+
+class Problem(models.Model):
+                question = models.CharField(max_length=255)
+
+                answer1 = models.CharField(max_length=255)
+                answer1_is_true = models.BooleanField(default=False)
+
+                answer2 = models.CharField(max_length=255)
+                answer2_is_true = models.BooleanField(default=False)
+                
+                answer3 = models.CharField(max_length=255)
+                answer3_is_true = models.BooleanField(default=False)
+
+                answer4 = models.CharField(max_length=255)
+                answer4_is_true = models.BooleanField(default=False)
